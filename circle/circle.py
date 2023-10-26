003d3d5976cca09a601935a1568eb132d7c9c6d2
Christian Federico Luppi <cluppi@payway.com.ar>
2023-10-26 20:48:38 -0300

    Generation method init, get_radius, set_radius, get_area

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,21 @@
 import math
 
 class Circle():
-    pass
+    def __init__(self,radius):
+        if radius <= 0:
+            raise ValueError(f"El radio del círculo debe ser un número mayor que 0 (cero) y positivo. Radio: {radius}")
+        self.radius = radius
+
+    
+    def get_radius(self):
+        return self.radius
+    
+
+    def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError(f"El radio del círculo debe ser un número mayor que 0 (cero) y positivo. Radio: {radius}")
+        self.radius = radius
+
+
+    def get_area(self):
+        return 3.14159 * self.radius ** 2
