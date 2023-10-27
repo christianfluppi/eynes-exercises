79ce2a10cb8ab028be027b59e6b8860d39e5194f
Christian Federico Luppi <cluppi@payway.com.ar>
2023-10-26 22:57:09 -0300

    Se agrega cacheo de error

--- simple/simple.py
+++ simple/simple.py
@@ -2,13 +2,17 @@ import random
 
 def simple_list():
     lista_de_diccionarios = []
-    for i in range(1, 11):
-        diccionario = {
-            "id": i,
-            "age": random.randint(1, 100) 
-        }
-        lista_de_diccionarios.append(diccionario)
-    return lista_de_diccionarios
+    try:
+        for i in range(1, 11):
+            diccionario = {
+                "id": i,
+                "age": random.randint(1, 100) 
+            }
+            lista_de_diccionarios.append(diccionario)
+        return lista_de_diccionarios
+    except (OSError, IOError) as e:
+        print(f"Error: {e}")
+        exit(1)
 
 def sort_list(dicts):
     sorted_list = sorted(dicts, key=lambda x: x["age"])
