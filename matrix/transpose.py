88b62cff051a16e1217a394b5ab9527f1a20ce3e
Christian Federico Luppi <cluppi@payway.com.ar>
2023-10-26 22:19:11 -0300

    Generate code Matrix

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -1,2 +1,7 @@
 def transpose(matrix):
-    pass
+    #Obtengo el numero de filas y columnas de la matriz
+    filas = len(matrix)
+    columnas = len(matrix[0])
+
+    #Inicializo la transpuesta con todos los valores en 0 (cero)
+    transpuesta = [[0 for _ in range(filas)] for _ in range(columnas)]
