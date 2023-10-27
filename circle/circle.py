2e5f99628c60ddfc797d7bc164c3eba6de943005
Christian Federico Luppi <cluppi@payway.com.ar>
2023-10-26 23:22:51 -0300

    Mejoras para Flake8

--- circle/circle.py
+++ circle/circle.py
@@ -10,7 +10,8 @@ class Circle():
     def get_radius(self): return self.radius
 
     def set_radius(self, radius):
-        if radius <= 0: raise ValueError(f"El radio del círculo debe ser mayor que 0 (cero) y positivo. Radio: {radius}")
+        if radius <= 0: 
+            raise ValueError(f"El radio del círculo debe ser mayor que 0 (cero) y positivo. Radio: {radius}")
         self.radius = radius
 
     def get_area(self): return math.pi * self.radius ** 2
@@ -18,7 +19,8 @@ class Circle():
     def get_perimeter(self): return 2 * math.pi * self.radius
 
     def __mul__(self, n):
-        if n <= 0: raise ValueError("El número de multiplicación debe ser mayor que cero.")
+        if n <= 0: 
+            raise ValueError("El número de multiplicación debe ser mayor que cero.")
         return Circle(self.radius * n)
 
     def __str__(self): return f"Círculo con radio: {self.radius}"
